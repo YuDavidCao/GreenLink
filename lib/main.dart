c288caad2820dee5f1ff8daafd2057ccdcc14ac5
YuDavidCao <yu.cao20041208@gmail.com>
2023-07-16 12:43:01 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'pages.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SolarGuide',
+        theme: ThemeData(
+          primarySwatch: const MaterialColor(0xFF0E2C71, {
+            50: Color(0xFF0E2C71),
+            100: Color(0xFF0E2C71),
+            200: Color(0xFF0E2C71),
+            300: Color(0xFF0E2C71),
+            400: Color(0xFF0E2C71),
+            500: Color(0xFF0E2C71),
+            600: Color(0xFF0E2C71),
+            700: Color(0xFF0E2C71),
+            800: Color(0xFF0E2C71),
+            900: Color(0xFF0E2C71),
+          }),
+        ),
+        initialRoute: '/HomePage',
+        routes: {
+          '/HomePage': (context) => const HomePage(),
+        },
+      ),
+    );
+  }
+}
